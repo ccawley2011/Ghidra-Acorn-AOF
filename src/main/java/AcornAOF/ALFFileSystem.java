517730dc5f61e7af9df7e286a7c1e0dab2eddb9d
Cameron Cawley <ccawley2011@gmail.com>
2022-10-27 16:57:26 +0100

    Update for compatibility with Ghidra 10.1

--- src/main/java/AcornAOF/ALFFileSystem.java
+++ src/main/java/AcornAOF/ALFFileSystem.java
@@ -15,16 +15,19 @@
  */
 package AcornAOF;
 
+import static ghidra.formats.gfilesystem.fileinfo.FileAttributeType.*;
+
 import java.io.*;
 import java.util.*;
 
 import ghidra.app.util.bin.BinaryReader;
 import ghidra.app.util.bin.ByteProvider;
-import ghidra.app.util.bin.ByteProviderInputStream;
+import ghidra.app.util.bin.ByteProviderWrapper;
 import ghidra.formats.gfilesystem.*;
 import ghidra.formats.gfilesystem.annotations.FileSystemInfo;
-import ghidra.formats.gfilesystem.factory.GFileSystemFactoryFull;
-import ghidra.formats.gfilesystem.factory.GFileSystemProbeFull;
+import ghidra.formats.gfilesystem.factory.GFileSystemFactoryByteProvider;
+import ghidra.formats.gfilesystem.factory.GFileSystemProbeByteProvider;
+import ghidra.formats.gfilesystem.fileinfo.FileAttributes;
 import ghidra.util.exception.CancelledException;
 import ghidra.util.task.TaskMonitor;
 
@@ -134,12 +137,11 @@ public class ALFFileSystem implements GFileSystem {
 	}
 
 	@Override
-	public InputStream getInputStream(GFile file, TaskMonitor monitor)
-			throws IOException, CancelledException {
-
+	public ByteProvider getByteProvider(GFile file, TaskMonitor monitor) {
 		ALFMetadata metadata = fsih.getMetadata(file);
 		return (metadata != null)
-				? new ByteProviderInputStream(provider, metadata.chunk.offset, metadata.chunk.size)
+				? new ByteProviderWrapper(provider, metadata.chunk.offset, metadata.chunk.size,
+					file.getFSRL())
 				: null;
 	}
 
@@ -149,28 +151,24 @@ public class ALFFileSystem implements GFileSystem {
 	}
 
 	@Override
-	public String getInfo(GFile file, TaskMonitor monitor) {
+	public FileAttributes getFileAttributes(GFile file, TaskMonitor monitor) {
 		ALFMetadata metadata = fsih.getMetadata(file);
-		return (metadata == null) ? null : FSUtilities.infoMapToString(getInfoMap(metadata));
+		FileAttributes result = new FileAttributes();
+		if (metadata != null) {
+			result.add(NAME_ATTR, metadata.name);
+			result.add(SIZE_ATTR, metadata.chunk.size);
+			result.add("Chunk index", metadata.chunkindex);
+		}
+		return result;
 	}
 
-	public Map<String, String> getInfoMap(ALFMetadata metadata) {
-		Map<String, String> info = new LinkedHashMap<>();
-
-		info.put("Name", metadata.name);
-		info.put("Size",
-			"" + Long.toString(metadata.chunk.size) + ", 0x" + Long.toHexString(metadata.chunk.size));
-		info.put("Chunk index", "" + Long.toString(metadata.chunkindex));
-		return info;
-	}
-
-	public static class ALFFileSystemFactory
-			implements GFileSystemFactoryFull<ALFFileSystem>, GFileSystemProbeFull {
+	public static class ALFFileSystemFactory implements
+			GFileSystemFactoryByteProvider<ALFFileSystem>, GFileSystemProbeByteProvider {
 
 		@Override
-		public ALFFileSystem create(FSRL containerFSRL, FSRLRoot targetFSRL,
-				ByteProvider byteProvider, File containerFile, FileSystemService fsService,
-				TaskMonitor monitor) throws IOException, CancelledException {
+		public ALFFileSystem create(FSRLRoot targetFSRL, ByteProvider byteProvider,
+				FileSystemService fsService, TaskMonitor monitor)
+				throws IOException, CancelledException {
 
 			ALFFileSystem fs = new ALFFileSystem(targetFSRL, byteProvider);
 			fs.mount(monitor);
@@ -178,9 +176,8 @@ public class ALFFileSystem implements GFileSystem {
 		}
 
 		@Override
-		public boolean probe(FSRL containerFSRL, ByteProvider byteProvider, File containerFile,
-				FileSystemService fsService, TaskMonitor monitor)
-				throws IOException, CancelledException {
+		public boolean probe(ByteProvider byteProvider, FileSystemService fsService,
+				TaskMonitor monitor) throws IOException, CancelledException {
 
 			AcornChunkParser parser = new AcornChunkParser(byteProvider);
 			return parser.isValid() && parser.contains("LIB_DIRY");
